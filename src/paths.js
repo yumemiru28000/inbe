6ba8fdfc6c0c046bd1da87a422bc5f9b5b1f26ce
yumemiru28000 <rigangyuu@gmail.com>
2026-01-21 04:33:27 +0900

    Update paths.js

--- src/paths.js
+++ src/paths.js
@@ -1,11 +1,13 @@
-
 export const PATHS = {
   presence: (uid) => `presence/${uid}`,
   names: (uid) => `lobby/names/${uid}`,
   leaderboardTop: `leaderboard/top`,
+
   sessionCurrent: `session/current`,
   sessionGame: `session/current/game`,
   sessionReady: (uid) => `session/current/ready/${uid}`,
   sessionRecruitingJoiner: `session/current/recruiting/joinerUid`,
-  submissions: (sessionId) => `submissions/${sessionId}`,
+
+  sessionInputs: (uid) => `session/current/inputs/${uid}`,
+  sessionClientState: (uid) => `session/current/clientState/${uid}`,
 };

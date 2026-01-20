8a3e880f80b4ecf965552a71fcc225c856979aaa
yumemiru28000 <rigangyuu@gmail.com>
2026-01-21 04:09:08 +0900

    Add PATHS object for URL path management

--- src/paths.js
+++ src/paths.js
@@ -1,1 +1,11 @@
 
+export const PATHS = {
+  presence: (uid) => `presence/${uid}`,
+  names: (uid) => `lobby/names/${uid}`,
+  leaderboardTop: `leaderboard/top`,
+  sessionCurrent: `session/current`,
+  sessionGame: `session/current/game`,
+  sessionReady: (uid) => `session/current/ready/${uid}`,
+  sessionRecruitingJoiner: `session/current/recruiting/joinerUid`,
+  submissions: (sessionId) => `submissions/${sessionId}`,
+};
